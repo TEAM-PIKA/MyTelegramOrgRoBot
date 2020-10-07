e84209ccde100ea3e64f3782e37020a64a8914a5
MR PIKACHU <72335343+TEAM-PIKA@users.noreply.github.com>
2020-10-07 18:55:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,11 +10,11 @@ now please send the Telegram code that you received from Telegram!
 
 this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/Thesanskariladka/MyTelegramOrgRoBot and clicking on the Pink Button
+by opening @Dark_cobra_support 😎
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@dark_cobra_support"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
